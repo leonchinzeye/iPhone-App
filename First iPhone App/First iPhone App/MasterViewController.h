67f5b9462fab8954dd062562ace2aa2708b7fe99
leonchinzeye <leonchin499@gmail.com>
2015-04-03 21:49:27 +0800

    Created and added first app files

---
+++ First iPhone App/First iPhone App/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  First iPhone App
+//
+//  Created by Chin Leon on 3/4/15.
+//  Copyright (c) 2015 Chin Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
